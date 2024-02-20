739925d325198e7d5f869d9a8e526029df288c5d
Vũ Trịnh <dragonking023@gmail.com>
2024-02-20 23:15:21 +0700

    test hello word

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,10 +1,8 @@
-import './App.css'
-
 function App() {
   return (
     <>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Hello Word
       </p>
     </>
   )
