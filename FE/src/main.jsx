4312a56d060fc649b191a4f79f7a0fd6a93c1889
Vũ Trịnh <dragonking023@gmail.com>
2024-03-20 05:33:16 +0700

    Replace relative import to absolute import

--- FE/src/main.jsx
+++ FE/src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "~/App.jsx";
 import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles";
-import theme from "./theme.js";
+import theme from "~/theme.js";
 import { CssBaseline } from "@mui/material";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
