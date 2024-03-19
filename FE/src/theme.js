d7dd3720b4be02baf661448c2bea9e45e02a50ad
Vũ Trịnh <dragonking023@gmail.com>
2024-03-20 06:27:10 +0700

    feat: design appbar

--- FE/src/theme.js
+++ FE/src/theme.js
@@ -3,8 +3,8 @@ import { cyan, deepOrange, orange, teal } from "@mui/material/colors";
 
 const theme = extendTheme({
   trello: {
-    appBarHeight: "48px",
-    boardBarHeight: "58px",
+    appBarHeight: "58px",
+    boardBarHeight: "60px",
   },
   colorSchemes: {
     light: {
